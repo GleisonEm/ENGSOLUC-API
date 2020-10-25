303d3f9f0f734e68a5f93e2d404b810e319f26a5
gleison1208 <gleison.spfc1208@hotmail.com>
2020-10-25 20:54:57 -0300

    add routes and gitignores

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () {
+	return response([
+		'message' => 'ENGSOLUC-API'
+	], 200);
+});
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+	$router->post('/generate/form', 'FormController@create');
+});
