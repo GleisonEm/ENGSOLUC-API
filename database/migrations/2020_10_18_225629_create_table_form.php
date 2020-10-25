adae83a2dbb60fa03e8a37428d5850a93ab874ec
gleison1208 <gleison.spfc1208@hotmail.com>
2020-10-25 20:54:08 -0300

    add database configs

---
+++ database/migrations/2020_10_18_225629_create_table_form.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableForm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('qone_enunciated');
+            $table->string('qone_response');
+            $table->string('qtwo_enunciated');
+            $table->string('qtwo_response');
+            $table->string('qthree_enunciated');
+            $table->string('qthree_response');
+            $table->string('qfour_enunciated');
+            $table->string('qfour_response');
+            $table->string('qfive_enunciated');
+            $table->string('qfive_response');
+            $table->string('qsix_enunciated');
+            $table->string('qsix_response');
+            $table->string('qseven_enunciated');
+            $table->string('qseven_response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formssee');
+    }
+}
